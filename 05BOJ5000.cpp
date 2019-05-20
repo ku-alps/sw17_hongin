1e484c75a0c245a58f7c9b46d0ceeab321882ca2
theorist <theorist17@gmail.com>
2019-05-20 13:35:07 +0900

    timeout

--- 05BOJ5000.cpp
+++ 05BOJ5000.cpp
@@ -2,24 +2,28 @@
 #include <deque>
 using namespace std;
 
-void rotate(deque<int*> &seq, int* ind) {
+void rotate(deque<int> &seq, int ind) {
 	int temp;
 	if (ind >= 2) {
-		temp = seq.at(ind)
-		seq.at(ind) = seq.at(ind-1)
-		seq.at(ind-1) = seq.at(ind-2)
-		seq.at(ind-2) = temp
+		temp = seq.at(ind);
+		seq.at(ind) = seq.at(ind-1);
+		seq.at(ind-1) = seq.at(ind-2);
+		seq.at(ind-2) = temp;
 	} else if (ind == 1) {
-		temp = seq.at(ind+1)
-		seq.at(ind+1 = seq.at(ind)
-		seq.at(ind) = seq.at(ind-1)
-		seq.at(ind-1) = temp
+		temp = seq.at(ind+1);
+		seq.at(ind+1)= seq.at(ind);
+		seq.at(ind) = seq.at(ind-1);
+		seq.at(ind-1) = temp;
 	} else if (ind == 0) {
-		temp = seq.at(ind+2)
-		seq.at(ind+2) = seq.at(ind+1)
-		seq.at(ind+1) = seq.at(ind)
-		seq.at(ind) = temp
+        temp = seq.at(ind+2);
+		seq.at(ind+2) = seq.at(ind+1);
+		seq.at(ind+1) = seq.at(ind);
+		seq.at(ind) = temp;
 	}
+
+	for (int i = 0 ; i < seq.size() ; i++)
+		cout << seq.at(i) << " ";
+	cout << endl;
 }
 int main()
 {
@@ -38,16 +42,18 @@ int main()
 	for (int i = 0 ; i < n ; i++) {
 		while(seq.front() != boss.at(i)) {
 			if (seq.size() >= 3) {
-				int i;
-				for (i = 0 ; i < n; i++)
-					if (seq.at(i) == boss.at(i))
+				int j;
+				for (j = 0 ; j < n; j++)
+					if (seq.at(j) == boss.at(i))
 						break;
-				rotate(seq, i);
+				rotate(seq, j);
 			} else {
 				cout << "Impossible";
+				return 0;
 			}
 		}
 		seq.pop_front();
 	}
 	cout << "Possible";	
+	return 0;
 }
