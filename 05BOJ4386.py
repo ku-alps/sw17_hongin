93c623771c2e78b70b71c612b20e13d1a80553a4
theorist <theorist17@gmail.com>
2019-05-27 19:30:38 +0900

    runtime error

--- 05BOJ4386.py
+++ 05BOJ4386.py
@@ -3,6 +3,7 @@ import math
 
 MAXDIST = 1415
 MINDIST = 1415
+
 def getDiff(apoint, otherpoint):
 	dist = math.sqrt((apoint[0]-otherpoint[0])**2 + (apoint[1]-otherpoint[1])**2)
 	return dist;
@@ -14,36 +15,39 @@ for i in range(n):
 	point = [int(value) for value in input().split() if value.isdigit()]
 	points.append(point)
 
-distances = []
+distances = [[getDiff(points[i], points[j]) for j in range(n)] for i in range(n)]
+initi = 0
+initj = 0
 for i in range(n):
-	row = []
 	for j in range(n):
-		if (i == j ):
-			temp = MAXDIST
-		else:
-			temp = getDiff(points[i], points[j])
-			row.append(temp)
-		print(temp, end = '\t\t')
-	distances.extend(row)
-	print()
+		if i == j:
+			distances[i][j] = MAXDIST
+		if distances[i][j] < MINDIST:
+			MINDIST = distances[i][j]
+			initi = i
+			initj = j
+#		print(distances[i][j], end='\t\t')
+#	print()
 
 ### initialization ###
-candidates = [int(i) for i in range(n)]
-for i in candidates:
-	print(i)
+constellation = [initi, initj]
+candidates = [i for i in range(n) if i not in constellation]
+#print(constellation)
+#print(candidates)
+distance = getDiff(points[initi], points[initj])
+star = 0
+while candidates:
+	MINDIST = MAXDIST
+	for i in constellation:
+		for j in candidates: 
+#			print("i : " + str(i) + ", j : " + str(j))
+			if (distances[i][j] < MINDIST):
+				MINDIST = distances[i][j]
+				star = j
+	candidates.remove(star)
+	constellation.append(star)
+	distance += MINDIST
+#	print("dist = " + str(distance))
 
-constellation = []
-mini = 0
-minj = 0
-for i in range(n):
-	for j in range(n):
-	if (distances[i][j] < MINDIST):
-			MINDIST = distances[i][j]
-			mini = i
-			minj = j
-
-constellation.append([mini, minj]	)
-distances[mini][minj] = MAXDIST
-distnaces[mini][minj] = MAXDIST
-	
-print(constellation[0])
+print(distance)
+#print(constellation)
