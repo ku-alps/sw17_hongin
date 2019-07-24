ef12bc31916046b76a961ad92394630d0ebf2540
theorist <theorist17@gmail.com>
2019-07-24 13:34:19 +0900

    디피꿀잼

--- 14BOJ9465스티커.cpp
+++ 14BOJ9465스티커.cpp
@@ -1,46 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int T, n, arr[2][100001], dp_sum[100000], dp_flg[100000], A, ans[100000];
+int T, n, arr[100001][2], dp[100001][3], ans[100000];
 
-void rcv_dp(int idx){
-    if (idx == n+1) {
-        if (A < dp_sum[n])
-            A = dp_sum[n];
-        return;
-    }
-    
-    if (dp_flg[idx-1] == 1 || dp_flg[idx-1] == 2) {
-        dp_flg[idx] = 0;
-        dp_sum[idx] = dp_sum[idx-1] + arr[0][idx];
-        rcv_dp(idx+1);
-    }
-    
-    if (dp_flg[idx-1] == 0 || dp_flg[idx-1] == 2) {
-        dp_flg[idx] = 1;
-        dp_sum[idx] = dp_sum[idx-1] + arr[1][idx];
-        rcv_dp(idx+1);
-    }
-    
-    if (dp_flg[idx-1] == 0 || dp_flg[idx-1] == 1) {
-        dp_flg[idx] = 2;
-        dp_sum[idx] = dp_sum[idx-1];
-        rcv_dp(idx+1);
-    }
-}
 int main() {
     cin >> T;
     for (int t = 0; t < T; t++){
         cin >> n;
-        A = 0;
-        dp_flg[0] = 2;
-        dp_sum[0] = 0;
         for (int i = 1; i <= n; i++)
-            cin >> arr[0][i];
+            cin >> arr[i][0];
         for (int i = 1; i <= n; i++)
-            cin >> arr[1][i];
-        rcv_dp(1);
-        ans[t] = A;
+            cin >> arr[i][1];
+        dp[0][0] = 0; dp[0][1] = 0; dp[0][2] = 0;
+        for (int i = 1; i <= n; i++){
+            dp[i][0] = max(dp[i-1][1] + arr[i][0], dp[i-1][2] + arr[i][0]);
+            dp[i][1] = max(dp[i-1][0] + arr[i][1], dp[i-1][2] + arr[i][1]);
+            dp[i][2] = max(dp[i-1][0], dp[i-1][1]);
+        }
+        ans[t] = max(max(dp[n][0], dp[n][1]), dp[n][2]);
     }
     for (int t = 0; t < T; t++){
         cout << ans[t] << endl;
