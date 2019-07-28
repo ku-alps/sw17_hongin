2d2958bd6c05c1b31173985a227c3c602965f1c4
theorist <theorist17@gmail.com>
2019-07-28 22:10:15 +0900

    칠전팔기. 스택을 조금만 쓰려다가 크게 고생했다.

--- 15BOJ9935문자열폭발.py
+++ 15BOJ9935문자열폭발.py
@@ -2,30 +2,30 @@ from collections import deque
 string = input()
 explosion = input()
 q = deque(maxlen=1000000) # stack
+s = deque(maxlen=1000000) # stack saving matches
 i = 0 # match counter
 l = len(explosion) # C4 --> 2
-s = deque() # stack saving matches
 for c in string:
-	if c == explosion[i]:
-		i += 1
-	else: # mismatch
-		if i > 0: # match was found (preceded)
-			s.append(i) # save to stack
-		if c == explosion[0]: # reset mach counter
-			i = 1
-		else:
-			i = 0
-	q.append(c)
-	print(s) 
-	print(q) 
-	if i == l: # match counter reached length of explosion string
-		for j in range(l): # pop as many as the length
-			q.pop() 
-		if s: 
-			i = s.pop()
-		else:
-			i = 0
+    if c == explosion[i]:
+        i += 1
+    else :
+        if c == explosion[0] :
+            i = 1
+        else :
+            i = 0
+    q.append(c)
+    s.append(i) # save to stack
+    #print(q)
+    #print(s)
+    if i == l: # match counter reached length of explosion string
+        for j in range(l): # pop as many as the length
+            q.pop()
+            i = s.pop()
+        if s:
+            i = s[-1]
+        else:
+            i = 0
 if q:
-	print(''.join(q))
+    print(''.join(q))
 else:
-	print("FRULA")
+    print("FRULA")
