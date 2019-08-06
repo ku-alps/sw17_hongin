73366116d9cd3bf6ffc10088191a1130eca65df5
theorist <theorist17@gmail.com>
2019-08-07 01:34:52 +0900

    끝내 푼 구현/시뮬... 개미를 못 풀어서 충격먹었지.

--- 16BOJ3048개미.cpp
+++ 16BOJ3048개미.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#define RIGHT 0
+#define LEFT 1
 using namespace std;
 
-void swap(char arr[26], int a, int b){
-    char t = arr[a];
+
+class Ants{
+public:
+    char ch;
+    int dir;
+};
+
+Ants arr[26];
+
+void swap(int a, int b){
+    Ants t = arr[a];
     arr[a] = arr[b];
     arr[b] = t;
 }
 
 int main(){
-    int N1, N2, T;
-    string s1, s2;
+    int N1, N2;
     cin >> N1 >> N2;
+    string s1, s2;
     cin >> s1 >> s2;
+    int T;
     cin >> T;
     
-    char* arr = new char[N1 + N2];
-    for(int i = 0 ; i < N1; i++)
-        arr[N1 -1 - i] = s1[i];
-    for(int i = 0 ; i < N2; i++)
-        arr[N1 + i] = s2[i];
-    for(int i = 0 ; i < N1 + N2; i++)
-        cout << arr[i] << endl;
+
+    for(int i = 0 ; i < N1; i++){
+        arr[N1 -1 - i].ch = s1[i];
+        arr[N1 -1 - i].dir = LEFT;
+    }
+
+    for(int i = 0 ; i < N2; i++){
+        arr[N1 + i].ch = s2[i];
+        arr[N1 + i].dir = RIGHT;
+    }
     
-    return 0;
-    
-    
+    for(int i = 0 ; i < T; i++) {
+        for ( int j = 0 ; j < N1 + N2 - 1 ; j++) {
+            if (arr[j].dir == LEFT && arr[j+1].dir == RIGHT){
+              swap(j, j+1);
+              j++;
+            }
+        }
+    }
+    for(int i = 0 ; i < N1 + N2; i++){
+        cout << arr[i].ch ;
+    }
     return 0;
 }
+
