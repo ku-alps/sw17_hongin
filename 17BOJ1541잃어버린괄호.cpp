a7cc509c57765d71b9d8f80baa79d8e2547905d4
theorist <theorist17@gmail.com>
2019-08-11 10:40:37 +0900

    그리디라고 분류. 그리디 아닌듯. 진작에 맞출수 있었어

--- 17BOJ1541잃어버린괄호.cpp
+++ 17BOJ1541잃어버린괄호.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
 string str;
+int val = 0;
 
+int read_next_num(int& i) {
+  int num = 0;
+  for (; i < str.length(); i++){
+    if (str[i] =='+' || str[i] == '-') {
+      break; 
+    } else {
+      num = 10 * num + (str[i]  - '0');
+    }
+  }
+  
+  return num;
+}
 int main() {
   getline(cin, str);
 
-  char buf[5];
-  int ind = 0;
-  int min = 0; 
-  //cout << "str len is " << str.length() << endl;
-  for (int i = 0 ; i < str.length() ; i++) {
-    if (str[i]=='-') {
-      int num;
-      cout << num << endl;
-      ind = 0;
-    } else if (str[i] == '+') {
+  bool found_first_minus = false;
+  
+  for (int i = 0 ; i < str.length(); i++){
+    // check for the '+' or '-'
+    if (found_first_minus) {
+      val -= read_next_num(i);
     } else {
+      val += read_next_num(i);
+      if (str[i] == '-') {
+        found_first_minus = true;
+      }
     }
   }
-    
-
-
+  cout << val << endl;
   return 0;
 }
