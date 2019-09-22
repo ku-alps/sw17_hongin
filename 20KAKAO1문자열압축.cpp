46e877dccd98802b1729a355e9f933becf53cc86
theorist <theorist17@gmail.com>
2019-09-22 16:36:23 +0900

    카카오

---
+++ 20KAKAO1문자열압축.cpp
@@ -0,0 +1,22 @@
+#include <iostream>>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int len = s.length();
+    int answer = l;
+
+    for (int l = 1 ; l <= len / 2; l++) {
+       for (int i = 0 ; i < n - l) {
+       }
+    }
+    return answer;
+}
+
+int main() {
+    string s = "aabbaccc";
+    cout << solution(s) << endl;
+    return 0;
+}
