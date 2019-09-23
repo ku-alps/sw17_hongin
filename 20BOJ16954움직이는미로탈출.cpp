6236b6ed83599f1998a622a10ebd39859d3c6c23
theorist <theorist17@gmail.com>
2019-09-23 13:51:03 +0900

    음.. 원경씨 도움이 좀 있었지만, 풀었잖아. 2시간?

--- 20BOJ16954움직이는미로탈출.cpp
+++ 20BOJ16954움직이는미로탈출.cpp
@@ -7,70 +7,86 @@ using namespace std;
 char map[8][8];
 pair<int, int> o;
 vector<pair<int, int> >visited;
-int x[9] = {0, -1, 0, 1, 1, 1, 0, -1, -1};
-int y[9] = {0, -1, -1, -1, 0, 1, 1, 1, 0};
+int x[9] = { 0, -1, 0, 1, 1, 1, 0, -1, -1 };
+int y[9] = { 0, -1, -1, -1, 0, 1, 1, 1, 0 };
 
 void print() {
-  for (int i = 0 ; i < 8 ; i++) {
-    for (int j = 0 ; j < 8; j++) {
-      printf("%c", map[i][j]);
-    }
-  }
-  cout << endl;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cout << map[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
 void fall() {
-  for (int i = 7; i >= 1; i--) {
-    for (int j = 0 ; j < 8; j++) {
-        map[i][j] = map[i-1][j];
-    }
-  }
-  for (int j = 0 ; j < 8; j++) {
-    map[0][j] = '.';
-  }
+	for (int i = 7; i >= 1; i--) {
+		for (int j = 0; j < 8; j++) {
+			map[i][j] = map[i - 1][j];
+		}
+	}
+	for (int j = 0; j < 8; j++) {
+		map[0][j] = '.';
+	}
 }
 
 int main() {
-  for (int i = 0 ; i < 8 ; i++) {
-    for (int j = 0 ; j < 8; j++) {
-      scanf("%c", &map[i][j]);
-    }
-  }
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cin >> map[i][j];
+		}
+	}
 
-  // bfs 
-  queue<pair<int, int> > q; 
-  q.push(make_pair(0, 7));
-  visited.push_back(make_pair(0, 7));
+	// bfs 
+	queue<pair<int, int> > q;
+	queue<int> d;
+	q.push(make_pair(0, 7));
+	d.push(0);
 
-  while(!q.empty()) {
-    // move
-    pair<int, int> t = q.front();
-    q.pop();
-    if (t.first == 7 && t.second == 0){
-      cout << 1 << endl;
-      return 0;
-    }
+	int round = 0;
+	pair<int, int> t;
+	int h;
+	int c, r;
 
-    cout << t.first << ", " << t.second << endl;
+	while (!q.empty()) {
+		// move
+		t = q.front();
+		h = d.front();
+		q.pop();
+		d.pop();
 
-    int c, r;
-    for (int i = 0 ; i < 9; i++ ){
-      c = t.first + x[i];
-      r = t.second + y[i];
-      if ( 0<= c && c<= 7 && 0<=r && r <=7){
-        if ( 0 < r && map[r-1][c] !='#') {
-          if (find(visited.begin(), visited.end(), make_pair(c, r)) == visited.end()){
-            q.push(make_pair(c, r));
-            visited.push_back(make_pair(c, r));
-          }
-        }
-      }
-    }
-    
-    // fall 
-    fall();
-  }
+		if (round == 7) {
+			cout << 1 << endl;
+			return 0;
+		}
+		if (t.first == 7 && t.second == 0) {
+			cout << 1 << endl;
+			return 0;
+		}
 
-  cout << '0' << endl;
-  return 0;
+		if (h > round) {
+			round++;
+			fall();
+			if (map[t.second][t.first] == '#')
+				continue;
+		}
+
+		//print();
+		//cout << t.first << ", " << t.second << " (depth " << h <<")"<< endl;
+
+		for (int i = 0; i < 9; i++) {
+			c = t.first + x[i];
+			r = t.second + y[i];
+			if (0 <= c && c <= 7 && 0 <= r && r <= 7) {
+				if (map[r][c] != '#' && 0 < r && map[r - 1][c] != '#') {
+					q.push(make_pair(c, r));
+					d.push(h + 1);
+				}
+			}
+		}
+	}
+
+	cout << '0' << endl;
+	return 0;
 }
